977e4614c6d6f5944af6c0ce66b3169acdd9fab3
Mohd Samgan Khan <mohdsamgankhan@gmail.com>
2020-01-08 12:37:40 +0400

    Updated auth role functions

--- src/HasRole.php
+++ src/HasRole.php
@@ -5,6 +5,7 @@ namespace Msamgan\Rpm;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
+use Msamgan\Rpm\Models\Role;
 use Msamgan\Rpm\Models\UserRole;
 
 /**
@@ -21,5 +22,46 @@ trait HasRole
         return UserRole::query()
             ->where('user_id', $this->id)
             ->first();
+    }
+
+    /**
+     * @param $name
+     * @return bool|Builder|Model
+     */
+    public function addRoleByName($name)
+    {
+        $role = Role::query()->where('name', $name)->first();
+        if ($role) {
+            return false;
+        }
+
+        return $this->addRole(Auth::id(), $role->id);
+    }
+
+    /**
+     * @param $userId
+     * @param $roleId
+     * @return Builder|Model
+     */
+    private function addRole($userId, $roleId)
+    {
+        return UserRole::query()->create([
+            'user_id' => $userId,
+            'role_id' => $roleId
+        ]);
+    }
+
+    /**
+     * @param $roleId
+     * @return bool|Builder|Model
+     */
+    public function addRoleById($roleId)
+    {
+        $role = Role::query()->find($roleId);
+        if ($role) {
+            return false;
+        }
+
+        return $this->addRole(Auth::id(), $role->id);
     }
 }
